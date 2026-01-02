17afaf006acdc0907097d95bf5cbbe20d529a627
ylim1699 <ylim.0348@gmail.com>
2026-01-01 22:47:19 -0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  base: "/workout/", 
+  root: "src/",
+
+  build: {
+    outDir: "../docs",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+      }
+    }
+  }
+});
